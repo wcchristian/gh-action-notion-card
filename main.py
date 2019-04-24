d34e76f1093bfac7b12f5963a509cb8283791e44
Christian Andersen <wcchristian@x-wing.local>
2019-04-23 21:22:56 -0500

    d

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from notion.block import TextBlock
 from notion.block import PageBlock
 import sys
 import os
+import json
 
 path = os.environ.get("GITHUB_EVENT_PATH")
 token = os.environ.get("NOTION_TOKEN")
 collectionUrl = os.environ.get("COLLECTION_URL")
-cardTitle = "This is the title of the github issue"
-cardDescription = "This is the content of the github issue"
-tagList = ["Github Issue", "SomeTag"]
 
 with open(path,"r") as f:
     contents = f.read()
 
+githubPayloadJson = json.loads(contents)
+
+cardTitle = githubPayloadJson["issue"]["title"]
+cardDescription = githubPayloadJson["issue"]["url"]
+tagList = ["Github Issue", "SomeTag"]
 
 client = NotionClient(token_v2=token)
 cv = client.get_collection_view(collectionUrl)
 row = cv.collection.add_row()
 row.name = cardTitle
-row.tag = tagList
-row.children.add_new(TextBlock, title=contents)
+row.children.add_new(TextBlock, title=cardDescription)
