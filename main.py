6602e393d464ada790eac60236659093a4b3d372
Christian Andersen <wcchristian@x-wing.local>
2019-04-23 21:05:33 -0500

    asf

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from notion.client import NotionClient
 from notion.block import TextBlock
+from notion.block import PageBlock
 import sys
 import os
 
+path = os.environ.get("GITHUB_EVENT_PATH")
 token = os.environ.get("NOTION_TOKEN")
 collectionUrl = os.environ.get("COLLECTION_URL")
 cardTitle = "This is the title of the github issue"
 cardDescription = "This is the content of the github issue"
 tagList = ["Github Issue", "SomeTag"]
 
-print(token)
-print(collectionUrl)
+with open(path,"r") as f:
+    contents = f.read()
 
 
 client = NotionClient(token_v2=token)
@@ -18,3 +20,4 @@ cv = client.get_collection_view(collectionUrl)
 row = cv.collection.add_row()
 row.name = cardTitle
 row.tag = tagList
+row.children.add_new(TextBlock, title=contents)
